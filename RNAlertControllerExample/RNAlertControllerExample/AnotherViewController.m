8aa2644be346d20cc7c080d6e33b8ff16a15b3d5
Rayhan Nabi <rayhan.nabi@iappdragon.com>
2019-08-29 12:40:34 +0600

    Add another view controller

---
+++ RNAlertControllerExample/RNAlertControllerExample/AnotherViewController.m
@@ -0,0 +1,21 @@
+//
+//  AnotherViewController.m
+//  RNAlertControllerExample
+//
+//  Created by Rayhan Nabi on 8/29/19.
+//  Copyright © 2019 Rayhan. All rights reserved.
+//
+
+#import "AnotherViewController.h"
+
+@interface AnotherViewController ()
+
+@end
+
+@implementation AnotherViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
