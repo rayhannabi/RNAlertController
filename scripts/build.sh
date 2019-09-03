91ecc50c4f243a37495283afadd3a1c7ea123b0f
Rayhan Nabi <rayhanjanam@gmail.com>
2019-09-04 01:38:25 +0600

    Update travis configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,11 @@ echo 'Building'
 
 set -o pipefail | xcodebuild -project RNAlertController.xcodeproj -scheme RNAlertController -configuration Release -sdk iphonesimulator CONFIGURATION_BUILD_DIR=builds clean build | xcpretty -c
 
-ls -al builds
+ARTIFACT=builds/RNAlertController.framework
+
+if test -f $ARTIFACT; then
+    echo $ARTIFACT
+    echo 'Compressing'
+else
+    exit(1)
+fi
