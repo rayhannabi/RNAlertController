211794052abccfd4ef38eb4ef84098489d892e86
Rayhan Nabi <rayhan.nabi@iappdragon.com>
2019-09-04 12:36:08 +0600

    Update travis configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,13 +4,15 @@ echo 'Building'
 
 set -o pipefail | xcodebuild -project RNAlertController.xcodeproj -scheme RNAlertController -configuration Release -sdk iphonesimulator CONFIGURATION_BUILD_DIR=builds clean build | xcpretty -c
 
-ARTIFACT=./builds/RNAlertController.framework
+ls -al builds
 
-if test -f $ARTIFACT; then
-    echo $ARTIFACT
-    echo 'Compressing'
-    zip -vr RNAlertController.framework.zip RNAlertController.framework/ -x "*.DS_Store"
-    ls -al builds
-else
-    exit 1
-fi
+# ARTIFACT=./builds/RNAlertController.framework
+
+# if test -f $ARTIFACT; then
+#     echo $ARTIFACT
+#     echo 'Compressing'
+#     zip -vr RNAlertController.framework.zip RNAlertController.framework/ -x "*.DS_Store"
+#     ls -al builds
+# else
+#     exit 1
+# fi
