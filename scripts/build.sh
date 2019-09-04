ee73c88c64e562d387eed658c51edff0edcb9517
Rayhan Nabi <rayhan.nabi@iappdragon.com>
2019-09-04 13:35:33 +0600

    Update travis configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # Xcode Build
+echo 
 echo 'Building'
 set -o pipefail | xcodebuild -project RNAlertController.xcodeproj -scheme RNAlertController -configuration Release -sdk iphonesimulator CONFIGURATION_BUILD_DIR=builds clean build | xcpretty -c
 
 # Zip The Framework
+echo
 echo 'Compressing Artifact'
 zip -vr builds/RNAlertController.framework.zip builds/RNAlertController.framework/ -x "*.DS_Store"
 
-# Show Build Artifacts
-echo 'Artifacts'
-ls builds
-
 # Calculate SHA1 Checksum
+echo
 echo 'Generating checksum'
 shasum -ba 1 builds/RNAlertController.framework.zip > builds/RNAlertController.framework.zip.sha1
+
+# Show Build Artifacts
+echo
+echo 'Artifacts'
+ls --color=AUTO builds
