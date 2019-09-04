3f53feab16813aadfe3beff1cbe5fd9d7617bf2a
Rayhan Nabi <rayhan.nabi@iappdragon.com>
2019-09-04 12:30:00 +0600

    Update travis configuration

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ echo 'Building'
 
 set -o pipefail | xcodebuild -project RNAlertController.xcodeproj -scheme RNAlertController -configuration Release -sdk iphonesimulator CONFIGURATION_BUILD_DIR=builds clean build | xcpretty -c
 
-ARTIFACT=builds/RNAlertController.framework
+ARTIFACT='builds/RNAlertController.framework'
 
 if test -f $ARTIFACT; then
     echo $ARTIFACT
